4843da4756c992877f6b981e2aa86f9b78ecad27
Aleksander Pascoal <aleksvp@MacBook-Air-de-Aleksander.local>
2020-05-15 17:32:28 -0300

    Muda endpoint para producao

--- src/services/Constantes.js
+++ src/services/Constantes.js
@@ -1,4 +1,5 @@
-const BASE_BACKEND_URL='http://localhost:3001/'
+const BASE_BACKEND_URL_OLD='http://localhost:3001/'
+const BASE_BACKEND_URL='http://vagas-backend.herokuapp.com/'
 const FALE_CONOSCO_EMAIL='aleksvp@gmail.com'
 const ERROR_SESSION_EXPIRED=440
 const MESSAGE='message'

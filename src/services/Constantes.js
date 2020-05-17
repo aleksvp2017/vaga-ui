3414b20eacc5926ae1c65e657868d40b9ee8d5b5
Aleksander Pascoal <aleksvp@MacBook-Air-de-Aleksander.local>
2020-05-17 17:11:41 -0300

    Configura url do backend automaticamente para desenv ou prod.

--- src/services/Constantes.js
+++ src/services/Constantes.js
@@ -1,10 +1,22 @@
-const BASE_BACKEND_URL_OLD='http://localhost:3001/'
-const BASE_BACKEND_URL='https://vagas-backend.herokuapp.com/'
+const BASE_BACKEND_URL_DESENV='http://localhost:3001/'
+const BASE_BACKEND_URL_PROD='https://vagas-backend.herokuapp.com/'
 const FALE_CONOSCO_EMAIL='aleksvp@gmail.com'
 const ERROR_SESSION_EXPIRED=440
 const MESSAGE='message'
 const ERROR='error'
+var BASE_BACKEND_URL = getBaseURL()
 
 export {
     BASE_BACKEND_URL, FALE_CONOSCO_EMAIL, ERROR_SESSION_EXPIRED, MESSAGE, ERROR
+}
+
+function getBaseURL() {
+    if (window.location.href.indexOf('localhost') > -1) {
+        console.log('Desenv')
+        return BASE_BACKEND_URL_DESENV
+    }
+    else {
+        console.log('Prod')
+        return BASE_BACKEND_URL_PROD
+    }
 }
